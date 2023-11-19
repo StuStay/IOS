2e440b3ec71f6fb76ab5c93ba88bcd0c88fa96f6
Yassine Ezzar <125025891+Yassine-Ezzar@users.noreply.github.com>
2023-11-19 09:53:18 +0100

    Update LogementService.swift

--- Logement/Services/LogementService.swift
+++ Logement/Services/LogementService.swift
@@ -1,4 +1,3 @@
-// LogementService.swift
 import Foundation
 
 enum LogementError: Error {
@@ -16,7 +15,7 @@ class LogementService {
     func addLogement(logement: Logement, completion: @escaping (Result<Logement, Error>) -> Void) {
         let urlString = baseURL
         guard let url = URL(string: urlString) else {
-            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
+            completion(.failure(LogementError.invalidURL))
             return
         }
         
@@ -28,7 +27,7 @@ class LogementService {
             let encoder = JSONEncoder()
             request.httpBody = try encoder.encode(logement)
         } catch {
-            completion(.failure(error))
+            completion(.failure(LogementError.decodingError))
             return
         }
         
@@ -47,7 +46,7 @@ class LogementService {
                 let newLogement = try JSONDecoder().decode(Logement.self, from: data)
                 completion(.success(newLogement))
             } catch {
-                completion(.failure(error))
+                completion(.failure(LogementError.decodingError))
             }
         }
         
@@ -80,7 +79,7 @@ class LogementService {
                 let logements = try JSONDecoder().decode([Logement].self, from: data)
                 completion(.success(logements))
             } catch {
-                completion(.failure(error))
+                completion(.failure(LogementError.decodingError))
             }
         }
         
@@ -115,151 +114,10 @@ class LogementService {
                 if let message = json?["message"] as? String {
                     completion(.success(message))
                 } else {
-                    // If there's no message, consider it as a failure
                     completion(.failure(LogementError.invalidResponse))
                 }
             } catch {
-                completion(.failure(error))
-            }
-        }
-
-        task.resume()
-    }
-}
-/*import Foundation
-
-enum LogementError: Error {
-    case invalidURL
-    case requestFailed
-    case invalidResponse
-    case decodingError
-}
-
-class LogementService {
-    static let shared = LogementService()
-
-    private let baseURL = "http://localhost:3000/api/logements"
-
-    func addLogement(logement: Logement, completion: @escaping (Result<Logement, Error>) -> Void) {
-        let urlString = baseURL
-        guard let url = URL(string: urlString) else {
-            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
-            return
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        do {
-            let encoder = JSONEncoder()
-            request.httpBody = try encoder.encode(logement)
-        } catch {
-            completion(.failure(error))
-            return
-        }
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-
-            guard let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(LogementError.invalidResponse))
-                return
-            }
-
-            guard (200...299).contains(httpResponse.statusCode) else {
-                completion(.failure(LogementError.invalidResponse))
-                return
-            }
-
-            do {
-                let newLogement = try JSONDecoder().decode(Logement.self, from: data!)
-                completion(.success(newLogement))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-
-        task.resume()
-    }
-
-    func getLogements(completion: @escaping (Result<[Logement], Error>) -> Void) {
-        let urlString = baseURL
-        guard let url = URL(string: urlString) else {
-            completion(.failure(LogementError.invalidURL))
-            return
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-
-            guard let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(LogementError.invalidResponse))
-                return
-            }
-
-            guard (200...299).contains(httpResponse.statusCode) else {
-                completion(.failure(LogementError.invalidResponse))
-                return
-            }
-
-            do {
-                let logements = try JSONDecoder().decode([Logement].self, from: data!)
-                completion(.success(logements))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-
-        task.resume()
-    }
-
-    func deleteLogement(logementID: String, completion: @escaping (Result<String, Error>) -> Void) {
-        let urlString = "\(baseURL)/\(logementID)"
-        guard let url = URL(string: urlString) else {
-            completion(.failure(LogementError.invalidURL))
-            return
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "DELETE"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-
-            guard let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(LogementError.invalidResponse))
-                return
-            }
-
-            guard (200...299).contains(httpResponse.statusCode) else {
-                completion(.failure(LogementError.invalidResponse))
-                return
-            }
-
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any]
-                if let message = json?["message"] as? String {
-                    completion(.success(message))
-                } else {
-                    completion(.failure(LogementError.invalidResponse))
-                }
-            } catch {
-                completion(.failure(error))
+                completion(.failure(LogementError.decodingError))
             }
         }
 
